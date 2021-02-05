e803e51d5cb842ea673d365c8625f9eae5f4ce13
MuhammedMushthaque <mushthquekpp682@gmail.com>
2021-02-05 15:48:46 +0530

    SwitchWithFunction

--- bin/switchwithfunction.dart
+++ bin/switchwithfunction.dart
@@ -1,3 +1,27 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  int day = 0;
+  var today = getToday(day);
+  print(today);
+}
+
+String getToday(day) {
+  switch (day) {
+    case 7:
+      return 'Monday';
+    case 6:
+      return 'Tuesday';
+    case 5:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 3:
+      return 'Friday';
+    case 2:
+      return 'Saturday';
+    case 1:
+      return 'Sunday';
+
+    default:
+      return 'Not a day';
+  }
 }
